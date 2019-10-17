ab187d721a624f09a1e5e824b4dc984c707c99ea
SimonVestman <simonw202@gmail.com>
2019-10-17 11:18:34 +0200

    Initial commit

---
+++ src/Gissatalet.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Gissatalet {
+    static void main (String [] args) {
+        double x = Math.random();
+        return x;
+        String gissning = JOptionPane.showInputDialog(null, "skriv din gissning här")
+    }
+}
