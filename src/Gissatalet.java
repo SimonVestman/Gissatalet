d01e0c2fc38ed4e83c7d2c5e72b1283483738abd
SimonVestman <simonw202@gmail.com>
2019-10-18 11:11:45 +0200

    Nu kan man gissa talet iallfall

--- src/Gissatalet.java
+++ src/Gissatalet.java
@@ -1,9 +1,30 @@
+/*
+Skapat 2019-10-17
+Ett program där man ska gissa talet.
+*/
 import javax.swing.*;
+import java.util.Random;
 
 public class Gissatalet {
-    static void main (String [] args) {
-        double x = Math.random();
-        return x;
-        String gissning = JOptionPane.showInputDialog(null, "skriv din gissning här")
-    }
+   public static void main (String [] args) {
+        /* Värde till variablerna */
+        Random rand = new Random();
+        int x = rand.nextInt(100);
+        x+=1;
+
+        String S = JOptionPane.showInputDialog(null, "Gissa på ett tal mellan 1 och 100");
+               int gissning = Integer.parseInt(S);
+
+               if(gissning == x){
+                    JOptionPane.showMessageDialog(null, "Du hade rätt!");
+               }    else{
+                   JOptionPane.showMessageDialog(null,"Du gissade inte rätt");
+               }
+                    while (gissning != x) {
+                       String T = JOptionPane.showInputDialog(null,"Fel svar gissa igen!");
+                                int Ny = Integer.parseInt(T);
+
+                    }
+
+   }
 }
